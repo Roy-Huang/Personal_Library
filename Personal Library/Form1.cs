fcb16022e8037e897dd99a168d6845db0088b580
unknown <roy60531@gmail.com>
2018-10-30 13:44:30 +0800

    Only_UI

---
+++ Personal Library/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Personal_Library
+{
+    public partial class Main_View : Form
+    {
+        public Main_View()
+        {
+            InitializeComponent();
+        }
+    }
+}
